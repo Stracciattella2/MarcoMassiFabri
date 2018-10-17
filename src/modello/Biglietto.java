47a6cf8ae31859a15270c65e27cc14cc2f1354b2
fabriHub <fabriziorozzo@gmail.com>
2018-10-17 17:32:06 +0200

    add Biglietto

---
+++ src/modello/Biglietto.java
@@ -0,0 +1,40 @@
+package modello;
+
+public class Biglietto {
+
+	private Long id;
+	private Long idPartita;
+	private Long idUtente;
+	private Double prezzo;
+	
+	//GET
+	public Long getId() {
+		return id;
+	}
+	public Long getIdPartita() {
+		return idPartita;
+	}
+	public Long getIdUtente() {
+		return idUtente;
+	}
+	public Double getPrezzo() {
+		return prezzo;
+	}
+	
+	//SET
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public void setIdPartita(Long idPartita) {
+		this.idPartita = idPartita;
+	}
+	public void setIdUtente(Long idUtente) {
+		this.idUtente = idUtente;
+	}
+	public void setPrezzo(Double prezzo) {
+		this.prezzo = prezzo;
+	}
+	
+	
+	
+}
