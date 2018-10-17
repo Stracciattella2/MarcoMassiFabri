1001340f0e273f53a7e56f1cadfa6f55a817c2ac
Stracciattella1 <g1878272@nwytg.net>
2018-10-17 17:25:38 +0200

    completed modello Utente

--- src/modello/Utente.java
+++ src/modello/Utente.java
@@ -7,4 +7,72 @@ public class Utente {
 	private String cognome;
 	private String codiceFIscale;
 	private int eta;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCognome() {
+		return cognome;
+	}
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	public String getCodiceFIscale() {
+		return codiceFIscale;
+	}
+	public void setCodiceFIscale(String codiceFIscale) {
+		this.codiceFIscale = codiceFIscale;
+	}
+	public int getEta() {
+		return eta;
+	}
+	public void setEta(int eta) {
+		this.eta = eta;
+	}
+	@Override
+	public String toString() {
+		return "Utente [id=" + id + ", nome=" + nome + ", cognome=" + cognome + ", codiceFIscale=" + codiceFIscale
+				+ ", eta=" + eta + "]";
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Utente other = (Utente) obj;
+		if (codiceFIscale == null) {
+			if (other.codiceFIscale != null)
+				return false;
+		} else if (!codiceFIscale.equals(other.codiceFIscale))
+			return false;
+		if (cognome == null) {
+			if (other.cognome != null)
+				return false;
+		} else if (!cognome.equals(other.cognome))
+			return false;
+		if (eta != other.eta)
+			return false;
+		if (id != other.id)
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+	
+	
 }
