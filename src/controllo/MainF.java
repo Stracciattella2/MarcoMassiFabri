bd36283a70d0952be9b455a8dcf76f2d6d960373
fabriHub <fabriziorozzo@gmail.com>
2018-10-18 11:04:00 +0200

    DAO biglietto

--- src/controllo/MainF.java
+++ src/controllo/MainF.java
@@ -12,36 +12,30 @@ public class MainF {
 
 	public static void main(String[] args) {
 
-		findAllParite();
-//		DAOUtenteImpl daoUtenteImpl = new DAOUtenteImpl();
-//		daoUtenteImpl.cercaUtenti();
+		
+		
+		addBiglietto();
+		
+		findBigliettoByID();
+		
+		findBigliettoByPartita();
+		
+		findBigliettoByUtente();
+		
+		updateBiglietto();
 		
 		findAllBiglietti();
-//		addBiglietto();
+		
+		deleteBiglietto();
+		
+		findAllBiglietti();
 		
 		
-		
-	}
-
-	private static void findAllParite() {
-		DAOPartitaImpl daoPartitaImpl = new DAOPartitaImpl();
-
-		List<Partita> partite;
-
-		try {
-			partite = daoPartitaImpl.findAll();
-
-			for (Partita partita : partite) {
-				System.out.println(partita);
-			}
-		} catch (DAOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
 	}
 	
 	private static void findAllBiglietti() {
+		System.out.println("\nFINDALL\n");
+		
 		DAOBigliettoImpl daoBigliettoImpl = new DAOBigliettoImpl();
 		
 		List<Biglietto> biglietti;
@@ -56,15 +50,14 @@ public class MainF {
 		} catch (DAOException e) {
 			e.printStackTrace();
 		}
-		
-		
 	}
 	
 	private static void addBiglietto() {
+		System.out.println("\nADD 17\n");
 		
 		Biglietto biglietto = new Biglietto();
 		
-		biglietto.setId(15L);
+		biglietto.setId(17L);
 		biglietto.setIdPartita(1L);
 		biglietto.setIdUtente(1L);
 		biglietto.setPrezzo(200D);
@@ -81,11 +74,91 @@ public class MainF {
 	}
 	
 	private static void findBigliettoByID() {
+		System.out.println("\nFINDBYID\n");
 		
+		DAOBigliettoImpl daoBigliettoImpl = new DAOBigliettoImpl();
+		
+		Biglietto biglietto;
+		
+		try {
+			biglietto = daoBigliettoImpl.findById(1L);
+			
+			System.out.println(biglietto);
+			
+		} catch (DAOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	private static void findBigliettoByPartita() {
+		System.out.println("\nFINDBYPARTITA\n");
 		
+		DAOBigliettoImpl daoBigliettoImpl = new DAOBigliettoImpl();
+		
+		List<Biglietto> biglietti;
+		
+		try {
+			biglietti = daoBigliettoImpl.findByPartita(1L);
+			
+			for(Biglietto biglietto : biglietti) {
+				System.out.println(biglietto);
+			}
+			
+		} catch (DAOException e) {
+			e.printStackTrace();
+		}
 	}
 
+	private static void findBigliettoByUtente() {
+		System.out.println("\nFINDBYUTENTE\n");
+		
+		DAOBigliettoImpl daoBigliettoImpl = new DAOBigliettoImpl();
+		
+		List<Biglietto> biglietti;
+		
+		try {
+			biglietti = daoBigliettoImpl.findByUtente(1L);
+			
+			for(Biglietto biglietto : biglietti) {
+				System.out.println(biglietto);
+			}
+			
+		} catch (DAOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void updateBiglietto() {
+		System.out.println("\nUPDATE 17\n");
+		
+		Biglietto biglietto = new Biglietto();
+		
+		biglietto.setId(17L);
+		biglietto.setIdPartita(2L);
+		biglietto.setIdUtente(1L);
+		biglietto.setPrezzo(200D);
+		
+		DAOBigliettoImpl daoBigliettoImpl = new DAOBigliettoImpl();
+		
+		try {
+			daoBigliettoImpl.update(biglietto);
+		} catch (DAOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private static void deleteBiglietto() {
+		System.out.println("\nDELETE 17\n");
+		
+		DAOBigliettoImpl daoBigliettoImpl = new DAOBigliettoImpl();
+		
+		try {
+			daoBigliettoImpl.delete(17L);
+		} catch (DAOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 }
